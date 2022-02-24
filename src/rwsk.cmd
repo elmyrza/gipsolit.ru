3ab18b37827eb2be193f29e952453d4a3273531d
elmyrza <48093800+elmyrza@users.noreply.github.com>
2022-02-24 17:59:59 +0600

    Add files via upload

---
+++ src/rwsk.cmd
@@ -0,0 +1,4 @@
+start D:\Koala/Koala.exe
+start D:\"Sublime Text"/sublime_text.exe
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" https://www.youtube.com/playlist?list=PL3LQJkGQtzc5G7wIQfVqBMEprmTKZIaXf
+browser-sync start --server --files "css/*.css, *.html"
