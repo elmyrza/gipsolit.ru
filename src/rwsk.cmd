48d75207eac8e4a5be179e885fb546d76c7c0c31
elmyrza <48093800+elmyrza@users.noreply.github.com>
2022-02-24 18:00:51 +0600

    Delete rwsk.cmd

--- src/rwsk.cmd
+++
@@ -1,4 +0,0 @@
-start D:\Koala/Koala.exe
-start D:\"Sublime Text"/sublime_text.exe
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome_proxy.exe" https://www.youtube.com/playlist?list=PL3LQJkGQtzc5G7wIQfVqBMEprmTKZIaXf
-browser-sync start --server --files "css/*.css, *.html"
